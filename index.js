2772ffee2ce611d689751cd58c566b3fb1881e95
Diana Badas <diana.badas@gmail.com>
2019-07-24 13:03:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Write your code in this file!
 function scuberGreetingForFeet(someNumber) {
-  
+
 }
 
 function ternaryCheckCity(someCity) {
-  
+
 }
 
 function switchOnCharmFromTip(tip) {
-  
+
 }
