c984e014323e539f81704afd9631c41c73db11ea
Diana Badas <diana.badas@gmail.com>
2019-07-24 13:09:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Write your code in this file!
 function scuberGreetingForFeet(someNumber) {
   let greeting
-  if (someNumber <= 400 ) {
+  if (someNumber <= 400) {
     greeting = "This one is on me!"
+  } else if (someNumber > 2000) {
+    greeting = "I will gladly take your thirty bucks."
+  } else if (someNumber > 2500) {
+    greeting = "No can do."
   }
   return greeting
 };
